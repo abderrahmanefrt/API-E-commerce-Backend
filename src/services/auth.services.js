961d10c1e2d8f74622ee0fe2e17e0335a8804777
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2026-04-01 22:56:09 +0100

    Service Auth

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,1 +1,24 @@
-const prisma = require('../config/db');
+const prisma = require('../config/db')
+
+
+const createuser = async (data)=>{
+
+  try{
+    return await prisma.user.create({data})
+
+  }catch(error){
+    throw new Error('Error creating user')
+  }
+}
+
+const finduserbyemail = async (email)=>{
+  try{
+    return await prisma.user.findUnique({where: {email}})
+  }catch(error){
+    throw new Error('Error finding user by email')
+  }
+}
+module.exports ={
+  createuser,
+  finduserbyemail
+}
